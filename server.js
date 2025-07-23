212879e27dda1e308433b7c8632bf7bb5fd93eb3
dmacedo <devinm3333@outlook.com>
2025-07-23 11:58:43 -0700

    added custom middleware

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-
 const authController = require('./controllers/auth.js');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const applicationsController = require('./controllers/applications.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI); // start connection to database
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
@@ -28,13 +31,18 @@ app.use(
   })
 );
 
+// Placed before route definitions to allow usage within
+app.use(passUserToView); // pass user info to EJS templates
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.use('/auth', authController);
+app.use('/auth', authController); // auth routes
+app.use(isSignedIn); // Placed AFTER routes to allow user to sign-in first
+app.use('/users/:userId/applications', applicationsController); // application routes
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
