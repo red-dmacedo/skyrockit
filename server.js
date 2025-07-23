701dbe2244d88d39657501e6f709aafb8517f81f
dmacedo <devinm3333@outlook.com>
2025-07-23 10:23:38 -0700

    removed vip-lounge

--- server.js
+++ server.js
@@ -34,16 +34,12 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
-
 app.use('/auth', authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
+// function deleteAllUsers(){
+//   const allUsers = ;
+// };
