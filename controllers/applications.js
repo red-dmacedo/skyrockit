68f499524d0a360fede9368e9f88f2e0c5a90d33
dmacedo <devinm3333@outlook.com>
2025-07-24 10:29:29 -0700

    completed new application functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -16,4 +16,21 @@ router.get('/', (req, res) => {
   };
 });
 
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  try{
+    const curUser = await User.findById(req.session.user._id);
+    curUser.applications.push(req.body);
+    await curUser.save();
+    res.redirect(`/users/${curUser._id}/applications`);
+  }
+  catch(err){
+    console.log(err);
+    res.redirect('/');
+  };
+});
+
 module.exports = router;
